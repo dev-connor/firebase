ad17271f2baca3bb35dbdf9bf44367e8de2cc35e
Jun <kimhyunjun576@gmail.com>
2021-05-06 18:32:59 +0900

    Delete SDK in google-signin.js

--- public/google-signin.js
+++ public/google-signin.js
@@ -1,25 +1,3 @@
-// These samples are intended for Web so this import would normally be
-// done in HTML however using modules here is more convenient for
-// ensuring sample correctness offline.
-
-
-// Docs: https://source.corp.google.com/piper///depot/google3/third_party/devsite/firebase/en/docs/auth/web/google-signin.md<script>
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyBmn3pvJ3E9Nofp8LpS_UeShLaqKglmZTc",
-    authDomain: "test-a19d6.firebaseapp.com",
-    databaseURL: "https://test-a19d6-default-rtdb.firebaseio.com",
-    projectId: "test-a19d6",
-    storageBucket: "test-a19d6.appspot.com",
-    messagingSenderId: "437803216708",
-    appId: "1:437803216708:web:25f0556124c5274efff111",
-    measurementId: "G-B8DPQVNV8V"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-
 function googleSignInPopup() {
 	
 	// [START auth_google_provider_create]
@@ -48,4 +26,3 @@ function googleSignInPopup() {
     });
   // [END auth_google_signin_popup]
 }
-
