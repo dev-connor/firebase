fc3ae9d2aaddb020edc9037727a73fbd60743019
Jun <kimhyunjun576@gmail.com>
2021-05-08 06:43:52 +0900

    Move signOut

--- src/index.js
+++ src/index.js
@@ -16,13 +16,3 @@ function authStateListener() {
   });
   // [END auth_state_listener]
 }
-
-function signOut() {
-  // [START auth_sign_out]
-  firebase.auth().signOut().then(() => {
-    // Sign-out successful.
-  }).catch((error) => {
-    // An error happened.
-  });
-  // [END auth_sign_out]
-}
